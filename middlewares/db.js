5191f8082205d75968dd52c6ddb8e3672eb46874
salman-itShaala <salman.shaikh@itshaala.com>
2025-06-18 20:00:36 +0530

    added login route, save todo route

--- middlewares/db.js
+++ middlewares/db.js
@@ -8,3 +8,12 @@ const userSchema = new Schema({
 
 export const User = model("User", userSchema);
 
+const todoSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    userId: { type: Schema.ObjectId, ref: "User", required: true }
+})
+
+export const Todo = model("Todo", todoSchema);
+
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { connect } from "mongoose";
-import { User } from "./db.js";
+import { Todo, User } from "./db.js";
 
 const dbUrl = "mongodb+srv://salmanshaikh:n72L9oN54khi0vBJ@cluster0.oqtuyuo.mongodb.net/todo-app";
 
@@ -33,6 +33,62 @@ app.post("/signup", async (req, res) => {
         console.log("Error occured in signup route", error);
         res.status(500).send({ message: "Internal server error" });
     }
+
+})
+
+
+app.post("/login", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!email || !password) {
+        res.status(400).json({ message: "All fields are required" });
+    }
+
+    try {
+        const user = await User.findOne({ email, password });
+
+        if (!user) {
+            res.status(404).json({ message: "Invalid credentials" });
+            return;
+        }
+
+        res.status(200).json({ message: "login successfull", user });
+
+    } catch (error) {
+        console.log("Error in login route", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+// route to save todo
+
+app.post("/save-todo", async (req, res) => {
+    const userId = req.body.userId;
+    const title = req.body.title;
+    const description = req.body.description;
+
+    if (!userId || !title || !description) {
+        res.status(400).json({ message: "Required all fields" });
+        return;
+    }
+
+    // id if user exists or not
+    // bad req
+
+
+    try {
+        const todo = new Todo({ title, description, userId });
+
+        const savedTodo = await todo.save();
+
+        res.status(201).json({ message: "Todo saved succesfully", savedTodo });
+
+    } catch (error) {
+        console.log("Error in save-todo route", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+
 
 })
 
